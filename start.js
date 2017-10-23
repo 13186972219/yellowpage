ecc2cbc0d91f49e737d9550f363b6b48618cf81e
ruibz <ruibz.b.zhang@hotmail.com>
2017-10-23 20:33:10 +0000

    add style.css

--- start.js
+++ start.js
@@ -9,17 +9,22 @@ class Link
     this.url = url;
   }
   toString(){
-    return "<a href=" + this.url + ">" + this.name + "</a>";
+      return "<a href=" + this.url + ">" + this.name + "</a>";
   }
 }
 
 class Td
 {
-  constructor(link){
+  constructor(link, table, col){
     this.link = link;
+    this.table = table;
+    this.col = col;
   }
   toString(){
-    return "<td>" + this.link.toString() + "</td>";
+    if (this.col % 7 % 2 == 0)
+      return "\n<td class='tdSingle" + this.table%3 + "'>" + this.link.toString() + "</td>";
+    else
+      return "\n<td class='tdDouble" + this.table%3 + "'>" + this.link.toString() + "</td>";
   }
 }
 
@@ -32,29 +37,33 @@ class Tr
     this.tds.push(td);
   }
   toString(){
-    var text = "<tr>";
+    var text = "\n<tr>";
     for (var i = 0; i < this.tds.length; i++) {
       text += this.tds[i].toString();
     }
-    text += "</tr>\n";
+    text += "\n</tr>\n";
     return text;
   }
 }
 
 class Table
 {
-  constructor(title){
+  constructor(title, id){
     this.title = title;
+    this.id = id;
     this.tds = [];
   }
   add(td){
     this.tds.push(td);
   }
+  table_id(){
+    return this.id;
+  }
   toString(){
     var trs = [];
-    var text = "<table><tr><th colspan='8'>" + this.title + "</th></tr>";
+    var text = "\n<table cellpadding='3' cellspacing='3'>\n<tr>\n<th colspan='7' class='th" + this.id%3 + "'>" + this.title + "</th>\n</tr>";
     for (var i = 0; i < this.tds.length; i++) {
-      if (i % 8 == 0) {
+      if (i % 7 == 0) {
         trs.push(new Tr());
       }
       var tr = trs[trs.length - 1];
@@ -68,36 +77,38 @@ class Table
   }
 }
 
-css_table = 'table{border-style: double; width: 100%;}\n';
-css_tr = 'tr{text-align: center; width: 100%; border-style: double;}\n';
-css_th = 'th{text-align: center;  text-color: red; width: 100%; border-style: double;}\n';
-css_td = 'td{text-align: center; text-color: blue; border-style: double; }\n';
-htmlBegin='<html><head><title>Yellow Page</title>\n<style>' + css_table + css_tr + css_th + css_td + '</style>\n</head><body>\n';
-htmlEnd='</body></html>';
 
 var app = express();
 
 app.get('/',  function(req, res) {
+  var cssData = fs.readFileSync('style.css').toString();
+  htmlBegin='<html><head><title>Yellow Page</title>\n<style>' + cssData + '</style>\n</head>\n<body>\n<center>\n';
+  htmlEnd='\n</center>\n</body>\n</html>';
   res.writeHead(200, {'Content-Type': 'text/html'});
   var contents = fs.readFileSync('page.conf').toString();
   var lines = contents.split("\n");
   var tables = new Array();
+  var table_id = 0;
+  var td_id = 0;
   for (var i = 0; i < lines.length; i++) {
     var line = lines[i];
     if (line.length < 10)
       continue;
     var items = line.split("->");
     if (items[0] == 'Title'){
-      tables.push(new Table(items[1]));
+      tables.push(new Table(items[1], table_id));
+      table_id+=1;
+      td_id=0;
       continue;
     }
     var currTable = tables[tables.length - 1];
-    currTable.add(new Td(new Link(items[0], items[1])));
+    currTable.add(new Td(new Link(items[0], items[1]), currTable.table_id(), td_id));
+    td_id+=1;
   }
 
   var text = "";
   for(var i = 0; i < tables.length; i++) {
-    text += tables[i].toString() + "<br>";
+    text += tables[i].toString() + "\n<br>\n";
   }
 
 //console.log(text);
